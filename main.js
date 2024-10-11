532a0d0a513e11d84176d35448c2246f2063a1df
Debora Dutra <dutradeborab@gmail.com>
2024-10-11 13:07:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//EXERCICIO 1 ///////////
+
+function myBtn(id, color) {
+    const btn = document.getElementById(id);
+    btn.style.background = color;
+}
+
+// EXERCICIO 2 ////////////
+function Paragraph() {
+    const newParagraph = document.createElement("btnParagraph");
+    newParagraph.textContent = 'É só mais um esforço para o curso de Front-end e serei um grande programador.'
+    }
+
+    const buttonP = document.getElementById('btnp');
+    let addP = document.createElement('p');
+    
+    function createP() {    
+        addP.innerText = 'É só mais um esforço para o curso de Front-end e serei um grande programador.'
+    }
+    document.body.appendChild(addP);
+    console.log(addP)
+
+// EXERCICIO 4 ////////////
+
+function aprendidoEweb() {
+    const items = document.querySelectorAll('li');
+}
+console.log (aprendidoEweb);
+
+function aprendidoEweb() {
+    
+  }
+
